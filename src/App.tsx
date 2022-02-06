99e876bf9dfaab4e628922db5ba02d976fc84f7c
Le Quan <minhquanle_2002@yahoo.com>
2022-02-05 18:20:33 -0800

    added connection to backend through sockets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './COMPONENTS/title_animation'
 import './App.css'
+import io from 'socket.io-client'
+
+
 
 function App() {
+
+  const [socket, setSocket] = useState<any>(null)
+
+  useEffect(() => {
+    setSocket(io('http://localhost:3001'))  // connect to server
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return
+
+    socket.on('connect', () => {
+      console.log('connected')  // get initial data
+    })
+
+  }, [socket])
+
   return (
     <div className='App'>
       <div className='header-container'>
